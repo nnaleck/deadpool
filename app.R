29dbc395424468e24794b25bb0206f9670e69776
nnaleck <ilyes_faicel.kamel@g.enp.edu.dz>
2021-10-11 17:29:37 +0200

    code formatting wip

--- app.R
+++ app.R
@@ -56,180 +56,202 @@ binarise = function(x){
 }
 
 # UI
-ui <- fluidPage( theme = bs_theme(bootswatch = "flatly", base_font = font_google("PT Serif", local = TRUE)),
-            navbarPage("Student Grade Prediction",
-                
-                tabPanel("Descriptive analysis",
-                     h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
-                        tabsetPanel(
-                            tabPanel("Data summary",
-                                     fluidRow(
-                                         column(6, 
-                                                h3("Student grade prediction dataset"),
-                                                textOutput(outputId = "resumeIt")
-                                                ),
-                                         column(6, 
-                                                tableOutput(outputId = "dict"))
-                                     )),
-                            tabPanel("Univariate analysis",
-                                     sidebarLayout(
-                                         sidebarPanel(
-                                             selectInput("select", label = h4("Select a feature for univariate analysis"),
-                                                         choices = sort(names(df)),
-                                                         selected=1
-                                             )
-                                         ),
-                                         mainPanel(
-                                             fluidRow(
-                                                 column(6, 
-                                                        plotlyOutput(outputId = "effectifsHist")),
-                                                 column(6, 
-                                                        plotlyOutput(outputId = "frequenceHist"))
-                                             ),
-                                             fluidRow(
-                                                 column(6, 
-                                                        plotlyOutput(outputId = "effectifsCumCurve")),
-                                                 column(6, 
-                                                        tableOutput(outputId = "tabStat"))
-                                             ) 
-                                         )
-                                     )
-                            ),
-                            tabPanel("Bivariate analysis",
-                                     sidebarLayout(
-                                         sidebarPanel(
-                                             h4("Select 2 features for bivariate analysis"),
-                                             selectInput("selectA", label = "First feature",
-                                                         choices = names(df),
-                                                         selected=1
-                                             ),
-                                             selectInput("selectB", label="Second feature",
-                                                         choices = sort(names(df)), 
-                                                         selected = 1)
-                                         ),
-                                         mainPanel(
-                                             fluidRow(
-                                                 column(6, fluidRow(
-                                                     column(12, plotlyOutput(outputId = "nuagePointsBiv")),
-                                                     column(4, offset = 3, textOutput("correlation"))
-                                                 )),
-                                                 column(6, 
-                                                        plotlyOutput(outputId = "histogrammeMod"))
-                                             )
-                                         )
-                                     )
-                            ),
-                            tabPanel("Table", dataTableOutput("table"), style = "font-size: 85%")
-                        )
-            ),
-            tabPanel("Variables impact on final Grade",
-                     h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
-                     tabsetPanel(
-                         tabPanel("Qualitative variables",
-                                  sidebarLayout(
-                                      sidebarPanel(
-                                          selectInput("selectqual", label = h4("Select a qualitative variable"),
-                                                      choices = categorical,
-                                                      selected=1
-                                          )
-                                      ),
-                                      mainPanel(   
-                                          fluidRow(
-                                              column(6, 
-                                                     plotlyOutput(outputId = "piechartYes")),
-                                              column(6, 
-                                                     plotlyOutput(outputId = "mboxplots"))
-                                          )
-                                      )
-                                  )
+ui <- fluidPage(
+    theme = bs_theme(bootswatch = "flatly", base_font = font_google("PT Serif", local = TRUE)),
+    navbarPage(
+        "Student Grade Prediction",
+        tabPanel(
+            "Descriptive analysis",
+            h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
+            tabsetPanel(
+                tabPanel(
+                    "Data summary",
+                    fluidRow(
+                        column(6, h3("Student grade prediction dataset"), textOutput(outputId = "resumeIt")),
+                        column(6, tableOutput(outputId = "dict"))
+                    )
+                ),
+                tabPanel(
+                "Univariate analysis",
+                     sidebarLayout(
+                         sidebarPanel(
+                             selectInput(
+                                 "select",
+                                 label = h4("Select a feature for univariate analysis"),
+                                 choices = sort(names(df)),
+                                 selected=1
+                             )
                          ),
-                         
-                         tabPanel("Quantitave variables", 
-                                  sidebarLayout(
-                                      sidebarPanel(
-                                          selectInput("selectquant", label = h4("Select a quantitative variable"),
-                                                      choices = setdiff(quan, 'G3'),
-                                                      selected=1
-                                          )
-                                      ),
-                                      mainPanel(
-                                        fluidRow(
-                                          column(12, plotlyOutput(outputId = "gradesCorrP"))
-                                        ),
-                                        fluidRow(
-                                          column(4, offset = 3, textOutput("gradesCorr")),
-                                          column(8, plotlyOutput(outputId = "boxplots"))
-                                        )
-                                      )
-                                  )
+                        mainPanel(
+                            fluidRow(
+                                column(6, plotlyOutput(outputId = "effectifsHist")),
+                                column(6, plotlyOutput(outputId = "frequenceHist"))
+                            ),
+                            fluidRow(
+                                column(6, plotlyOutput(outputId = "effectifsCumCurve")),
+                                column(6, tableOutput(outputId = "tabStat"))
+                            )
+                        )
+                    )
+                ),
+                tabPanel("Bivariate analysis",
+                    sidebarLayout(
+                        sidebarPanel(
+                            h4("Select 2 features for bivariate analysis"),
+                            selectInput(
+                                "selectA",
+                                label = "First feature",
+                                choices = names(df),
+                                selected=1
+                            ),
+                            selectInput(
+                                "selectB",
+                                label="Second feature",
+                                choices = sort(names(df)),
+                                selected = 1
+                            )
                         ),
-                        tabPanel("Analyse according to tenure",
-                                 fluidRow(
-                                     column(12, plotlyOutput(outputId = "yearsNum")),
-                                 ),
-                                 fluidRow(
-                                   column(12, plotlyOutput(outputId = "yearsInc"))
-                                 )
-                                 
+                        mainPanel(
+                            fluidRow(
+                                column(
+                                    6,
+                                    fluidRow(
+                                        column(8, plotlyOutput(outputId = "nuagePointsBiv")),
+                                        column(4, offset = 3, textOutput("correlation"))
+                                    )
+                                ),
+                                column(6, plotlyOutput(outputId = "histogrammeMod"))
+                            )
                         )
-                     )
-            ),
-            tabPanel("Apprentissage supervisé", 
-                     h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
-                     tabsetPanel(
-                         tabPanel("k nearest neighbors",
-                                  sidebarLayout(
-                                  sidebarPanel(
-                                    sliderInput('k', 'Select the Number K of Nearest Neighbours', value = 6, min = 1, max = 100),
-                                    checkboxInput('balance', label = "Balance data (only for training)")
-                                  ),
-                                  mainPanel(
-                                      fluidRow(
-                                          column(6, 
-                                                 plotlyOutput(outputId = "boxplotAcc")
-                                                 ),
-                                          column(6, 
-                                                 plotOutput(outputId = "ROC")
-                                                 )
-                                          )   
-                                      )   
-                                  )
-                         ),
-                         tabPanel("Logistic regression",
-                                  checkboxInput('balance2', label = "Balance data (only for training)"),
-                                  fluidRow(
-                                      column(12,
-                                             plotlyOutput(outputId = "boxplotAcc2")
-                                      )
-                                  )   
-                         )   
-                         )
-                         ),
-                       tabPanel('Unsupervised Learning',
-                                tabsetPanel(
-                                  tabPanel('KMeans Clustering',
-                                           sidebarLayout(
-                                             sidebarPanel(
-                                               sliderInput('nb_clusters', 'Number of clusters', value = 3, min = 2, max = 7),
-                                             ),
-                                             mainPanel(
-                                               fluidRow(
-                                                 column(12, plotOutput(outputId = "clustering_plot"))
-                                               )
-                                             )
-                                           )
-                                  ),
-                                  tabPanel('Principal Component Analysis',
-                                         mainPanel(
-                                           fluidRow(
-                                             column(12, plotlyOutput(outputId = "pca_plot"))
-                                           )
-                                         )
-                                  )
-                                )
-                       )
+                    )
+                ),
+                tabPanel("Table", dataTableOutput("table"), style = "font-size: 85%")
             )
-        )
+        ),
+        tabPanel(
+            "Variables impact on final Grade",
+            h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
+             tabsetPanel(
+                tabPanel(
+                    "Qualitative variables",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput(
+                                "selectqual",
+                                label = h4("Select a qualitative variable"),
+                                choices = categorical,
+                                selected=1
+                            )
+                        ),
+                        mainPanel(
+                            fluidRow(
+                                column(6, plotlyOutput(outputId = "piechartYes")),
+                                column(6, plotlyOutput(outputId = "mboxplots"))
+                            )
+                        )
+                    )
+                ),
+                tabPanel(
+                    "Quantitave variables",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput(
+                                "selectquant",
+                                label = h4("Select a quantitative variable"),
+                                choices = setdiff(quan, 'G3'),
+                                selected=1
+                            )
+                        ),
+                        mainPanel(
+                            fluidRow(
+                                column(12, plotlyOutput(outputId = "gradesCorrP"))
+                            ),
+                            fluidRow(
+                                column(4, offset = 3, textOutput("gradesCorr")),
+                                column(8, plotlyOutput(outputId = "boxplots"))
+                            )
+                        )
+                    )
+                ),
+                tabPanel(
+                    "Analyse according to tenure",
+                    fluidRow(
+                        column(12, plotlyOutput(outputId = "yearsNum")),
+                    ),
+                    fluidRow(
+                        column(12, plotlyOutput(outputId = "yearsInc"))
+                    )
+                )
+             )
+        ),
+        tabPanel(
+            "Apprentissage supervisé",
+            h4("Travail realisé par Ilyes Kamel, Abdelkarim Azzaz et Achraf Louiza"),
+            tabsetPanel(
+                tabPanel(
+                    "KNNs",
+                    sidebarLayout(
+                        sidebarPanel(
+                            sliderInput(
+                                'k',
+                                'Select the Number K of Nearest Neighbours',
+                                value = 6,
+                                min = 1,
+                                max = 100
+                            ),
+                            checkboxInput('balance', label = "Balance data (only for training)")
+                        ),
+                        mainPanel(
+                            fluidRow(
+                                column(6, plotlyOutput(outputId = "boxplotAcc"))
+                            ),
+                            column(6, plotOutput(outputId = "ROC"))
+                        )
+                    )
+                ),
+                tabPanel(
+                    "Logistic regression",
+                    checkboxInput('balance2', label = "Balance data (only for training)"),
+                     fluidRow(
+                         column(12, plotlyOutput(outputId = "boxplotAcc2"))
+                     )
+                )
+            ),
+        ),
+       tabPanel(
+           'Unsupervised Learning',
+            tabsetPanel(
+                tabPanel(
+                    'KMeans Clustering',
+                    sidebarLayout(
+                        sidebarPanel(
+                            sliderInput(
+                                'nb_clusters',
+                                'Number of clusters',
+                                value = 3,
+                                min = 2,
+                                max = 7
+                            )
+                        ),
+                        mainPanel(
+                            fluidRow(
+                                column(12, plotOutput(outputId = "clustering_plot"))
+                            )
+                        )
+                    )
+                ),
+                tabPanel(
+                    'Principal Component Analysis',
+                    mainPanel(
+                        fluidRow(
+                            column(12, plotlyOutput(outputId = "pca_plot"))
+                        )
+                    )
+                )
+            )
+       )
+    )
+)
 
 
 
